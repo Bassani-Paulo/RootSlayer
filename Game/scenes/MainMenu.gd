01b480da31f2d45fe7c7a30d6997381fce7dfd72
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-02-05 16:38:58 -0300

    better credits

--- Game/scenes/MainMenu.gd
+++ Game/scenes/MainMenu.gd
@@ -4,12 +4,14 @@ signal change_scene(scene_type)
 
 func _ready():
 	$"Credits Text".visible = false
+	$"Credits2".visible = false
 
 func _on_Exit_pressed():
 	get_tree().quit(0)
 
 func _on_Credits_pressed():
 	$"Credits Text".visible = not $"Credits Text".visible
+	$"Credits2".visible = not $"Credits2".visible
 
 func _on_Play_pressed():
 	emit_signal("change_scene", Global.SceneType.Game)

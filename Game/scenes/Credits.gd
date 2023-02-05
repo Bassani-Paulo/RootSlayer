a77b2949a370258f9e9ca3fd2a34e452ef477f26
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-02-05 14:20:10 -0300

    improve main menu

--- Game/scenes/Credits.gd
+++
@@ -1,4 +0,0 @@
-extends MarginContainer
-
-func _on_RichTextLabel_meta_clicked(meta):
-	OS.shell_open(meta)
--- Game/scenes/MainMenu.gd
+++ Game/scenes/MainMenu.gd
@@ -1,17 +1,9 @@
-extends MarginContainer
+extends Node2D
 
 signal change_scene(scene_type)
 
-var _version
-
 func _ready():
-	var file = File.new()
-	file.open("res://version.txt", File.READ)
-	_version = file.get_line()
-	file.close()
-	$VBoxContainer/VersionLabel.text = 'v' + _version
-	$VBoxContainer/HBoxContainer/MenuContainer/VBoxContainer/Play.grab_focus()
-
+	pass
 
 func _on_Exit_pressed():
 	emit_signal("change_scene", Global.SceneType.Quit)
@@ -20,4 +12,4 @@ func _on_Help_pressed():
 	emit_signal("change_scene", Global.SceneType.Help)
 
 func _on_Play_pressed():
-	emit_signal("change_scene", Global.SceneType.SelectLevel)
+	emit_signal("change_scene", Global.SceneType.Game)

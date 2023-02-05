34523bcce8cb1b1d2958abe4204489de76637f41
Igor Borges <devatdevian@gmail.com>
2023-02-04 21:48:35 -0300

    move and attack animations

--- Game/Player Prop.gd
+++ Game/Player Prop.gd
@@ -5,18 +5,36 @@ const speed = 360
 const type = "player"
 var teste = 0
 var axe = preload("res://Gun.tscn")
+var animation_tree
+var animation_state_machine
+
 func _ready():
-	pass
+	animation_tree = $AnimationTree
+	animation_state_machine = $AnimationTree.get("parameters/playback")
+	animation_state_machine.travel("Idle")
 
 func _process(delta):
-	if Input.is_action_pressed("move_left"):
-		self.position.x -= speed * delta
-	if Input.is_action_pressed("move_right"):
-		self.position.x += speed * delta
-	if Input.is_action_pressed("move_up"):
-		self.position.y -= speed * delta
-	if Input.is_action_pressed("move_down"):
-		self.position.y += speed * delta
+	var _movement_vector = Vector2(0,0)
+
+	if Input.is_action_pressed("attack"):
+		animation_state_machine.travel("AxeUpAttack")
+
+	else:
+		if Input.is_action_pressed("move_left"):
+			_movement_vector.x = -1
+		if Input.is_action_pressed("move_right"):
+			_movement_vector.x = 1
+		if Input.is_action_pressed("move_up"):
+			_movement_vector.y = -1
+		if Input.is_action_pressed("move_down"):
+			_movement_vector.y = 1
+		
+		if _movement_vector == Vector2(0,0):
+			animation_state_machine.travel("Idle")
+		else:
+			animation_state_machine.travel("Move")
+
+		self.position += (_movement_vector.normalized()) * speed * delta
 
 func _on_Area2D_area_entered(area):
 	if area.get_parent().type == "enemy":
